a854819eded7e3bbbf2c2b6a7b5b16bdb3e24401
Mean <390602272@qq.com>
2020-06-08 10:08:39 +0800

    Complete HMAC-SHA-1 and first birthday attack

---
+++ src/FirstBirthdayAttack.java
@@ -0,0 +1,67 @@
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * SHA-1第一类生日攻击
+ */
+public class FirstBirthdayAttack {
+    /**
+     * 第一类生日攻击
+     * 
+     * @param h 要攻击的hash值
+     * @param n 相同位数(前n位)
+     * @return 前n位相同的一个消息
+     */
+    public static byte[] attack(byte[] h, int n) {
+        Random random = new Random();
+        BigInteger a;
+        byte[] b;
+        byte[] h1;
+        // 不断随机产生消息，计算hash值，和给出的hash值对比
+        while (true) {
+            a = new BigInteger(240, random);
+            b = a.toByteArray();
+            h1 = SHA1.hash(b);
+            if (sameBits(h, h1) >= n)
+                return b;
+        }
+    }
+
+    /**
+     * 计算从最高位开始连续相同的位数
+     */
+    private static int sameBits(byte[] a, byte[] b) {
+        byte c;
+        int n = 0;
+        for (int i = 0; i < a.length; i++) {
+            c = (byte) (a[i] ^ b[i]);
+            //若c的某一位为0，则a和b的对应位相同
+            for (int j = 7; j >= 0; j--) {
+                if (c >> j == 0)
+                    n++;
+                else
+                    return n;
+            }
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hash:");
+        byte[] h = new byte[20];
+        char[] c = scanner.next().toCharArray();
+        for (int i = 0; i < 20; i++)
+            h[i] = (byte) Integer.parseInt(String.valueOf(c, 2 * i, 2), 16);
+        System.out.print("length of same bits:");
+        int n = scanner.nextInt();
+        byte[] b = attack(h, n);
+        byte[] h1 = SHA1.hash(b);
+        System.out.printf("message with same %d bits:\n", n);
+        System.out.println(new BigInteger(1, b).toString(16));
+        System.out.println("hash:");
+        System.out.println(new BigInteger(1, h1).toString(16));
+        scanner.close();
+    }
+}
---
+++ src/HMACSHA1.java
@@ -0,0 +1,66 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+/**
+ * HMAC-SHA-1
+ */
+public class HMACSHA1 {
+    /**
+     * 加密
+     * 
+     * @param m 消息
+     * @param k 密钥
+     * @return MAC
+     */
+    public static byte[] encrypt(byte[] m, byte[] k) {
+        // 对k进行填充得到k+
+        byte[] kPlus = paddingK(k);
+        byte[] si = new byte[64];
+        // si=k+ ⊕ ipad
+        for (int i = 0; i < 64; i++)
+            si[i] = (byte) (kPlus[i] ^ 0x36);
+        // in1=si||m
+        byte[] in1 = new byte[64 + m.length];
+        System.arraycopy(si, 0, in1, 0, 64);
+        System.arraycopy(m, 0, in1, 64, m.length);
+        // h = H(si||m)
+        byte[] h = SHA1.hash(in1);
+        byte[] s0 = new byte[64];
+        // s0=k+ ⊕ opad
+        for (int i = 0; i < 64; i++)
+            s0[i] = (byte) (kPlus[i] ^ 0x5c);
+        // in2=s0||h
+        byte[] in2 = new byte[84];
+        System.arraycopy(s0, 0, in2, 0, 64);
+        System.arraycopy(h, 0, in2, 64, 20);
+        // 输出H(s0||h)
+        return SHA1.hash(in2);
+    }
+
+    /**
+     * 对k进行填充
+     */
+    private static byte[] paddingK(byte[] k) {
+        byte[] kPlus = new byte[64];
+        // 如果k超过512位，令k=H(k)
+        if (k.length > 64)
+            k = SHA1.hash(k);
+        // 在k左边填充0，得到512位k+
+        System.arraycopy(k, 0, kPlus, 64 - k.length, k.length);
+        return kPlus;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("message:");
+        byte[] m = scanner.nextLine().getBytes();
+        System.out.println("k:");
+        char[] c = scanner.next().toCharArray();
+        byte[] k = new byte[c.length / 2];
+        for (int i = 0; i < k.length; i++)
+            k[i] = (byte) Integer.parseInt(String.valueOf(c, 2 * i, 2), 16);
+        byte[] h = encrypt(m, k);
+        System.out.println(new BigInteger(1, h).toString(16));
+        scanner.close();
+    }
+}
